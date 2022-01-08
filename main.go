715a39ec837be234fd635e39668fe5a5c24e61dd
AntonyIS <antony@ariyafinergy.com>
2022-01-08 09:48:02 +0300

    Added Two Sum challenge

--- main.go
+++ main.go
@@ -7,36 +7,8 @@ import (
 )
 
 func main() {
-	sorted_list := []int{1, 2, 4, 5, 6, 7, 8, 9}
-	unsorted_list := []int{9, 2, 6, 1, 3, 7, 5}
-
-	res := solutions.Merge_sort(unsorted_list)
+	nums := []int{2, 7, 11, 15}
+	target := 9
+	res := solutions.TwoSum(nums, target)
 	fmt.Println(res)
-	// linear_search
-	ok, err := solutions.Linear_search(sorted_list, 9)
-	fmt.Println("LINEAR SEARCH")
-	if err != nil {
-		fmt.Println("Error :", err)
-	} else {
-		fmt.Println(ok)
-	}
-	// binary_search
-	fmt.Println("BINARY SEARCH")
-	pass, err := solutions.Binary_search(sorted_list, 9)
-
-	if err != nil {
-		fmt.Println("Error :", err)
-	} else {
-		fmt.Println(pass)
-	}
-
-	// binary_search
-	fmt.Println("RECURSIVE BINARY  SEARCH")
-	check, err := solutions.Recursive_binary_search(sorted_list, 9)
-
-	if err != nil {
-		fmt.Println("Error :", err)
-	} else {
-		fmt.Println(check)
-	}
 }
---
+++ solutions/twoSum.go
@@ -0,0 +1,36 @@
+package solutions
+
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order.
+*/
+
+func TwoSum(nums []int, target int) []int {
+	// Solution 1 : 0(n2)
+	// for i := 0; i < len(nums)-1; i++ {
+	// 	for j := 0; j < len(nums); j++ {
+	// 		if nums[i]+nums[j] == target {
+	// 			return []int{i, j}
+	// 		}
+	// 	}
+	// }
+
+	// Solution 2 : 0(1)
+
+	// Map to store complement keys
+	mapper := make(map[int]int)
+	for i, value := range nums {
+		// Get complement
+		complement := target - value
+		if _, ok := mapper[complement]; ok {
+			// Return the index of the 1st and 2nd value that sum up to target
+			return []int{mapper[value], i}
+		} else {
+			// Store value as the key and index as a value for reference
+			mapper[value] = i
+		}
+	}
+	return []int{}
+
+}
