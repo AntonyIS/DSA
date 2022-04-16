33c191abaed7c1d620038d65469c36166f659bd8
AntonyIS <antony@ariyafinergy.com>
2022-04-16 21:14:03 +0300

    Adding length method

--- LinkedList/main.go
+++ LinkedList/main.go
@@ -4,6 +4,9 @@ import "fmt"
 
 func main() {
 	fmt.Println("Welcome to Go LinkedList")
+	ll := LinkedList{}
+	fmt.Println(ll.is_empty()) // true
+	fmt.Println(ll.length())   // 0
 }
 
 // Create a node : A node should have a data and a pointer to the next node if any else points to nil.
@@ -23,4 +26,25 @@ func (ll LinkedList) is_empty() bool {
 	// Checks if a LinkedList is empty or not
 	// Returns a boolean true if empty else returns false
 	return ll.head == nil // if LinkedList head is nill, the LinkedList is empty
+}
+
+// Get the length of a LinkedList
+func (ll LinkedList) length() int {
+	// Returns the length(int) of items in a LinkedList
+	// This a linear operation 0(n)
+	count := 0 //Initial length of a LinkedList
+
+	current := ll.head // Keep track of the current node in a LinkedList
+
+	// Loop through LinkedList until we get to the tail node and return count
+	for current != nil {
+		// As long as the next_node is not nil
+		// Increament count
+		count += 1
+		// Move current node to the next node
+		current = current.next_node
+	}
+
+	return count
+
 }
